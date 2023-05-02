3dafd9fe589e547027fb3015f6d1defcb6f9b5e8
miguelgarciak <miguel@eucalyptuslabs.com>
2023-05-02 08:59:06 -0600

    Add custom overlay (WIP)

--- lib/pages/home/widgets/derived_account_item.dart
+++ lib/pages/home/widgets/derived_account_item.dart
@@ -38,13 +38,8 @@ class DerivedAccountItem extends StatelessWidget {
                   SizedBox(width: 80.w),
                   MouseRegion(
                     cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () async {
-                        await Clipboard.setData(
-                          ClipboardData(text: result.account),
-                        );
-                        toast(Strings.copiedToClipboard);
-                      },
+                    child: CustomToast(
+                      content: result.account,
                       child: Text(result.account),
                     ),
                   ),
@@ -70,4 +65,76 @@ class DerivedAccountItem extends StatelessWidget {
           ],
         ),
       );
+}
+
+class CustomToast extends StatefulWidget {
+  final Widget child;
+  final String content;
+
+  const CustomToast({
+    required this.child,
+    required this.content,
+    super.key,
+  });
+
+  @override
+  State<CustomToast> createState() => _CustomToastState();
+}
+
+class _CustomToastState extends State<CustomToast> {
+  OverlayEntry? _overlayEntry;
+
+  @override
+  void dispose() {
+    _overlayEntry?.remove();
+    super.dispose();
+  }
+
+  void _showToast(BuildContext context, Offset position) {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        left: position.dx + 30,
+        top: position.dy + 30,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: const Text(
+              'Copied to clipboard',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        _overlayEntry?.remove();
+        _overlayEntry = null;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (details) async {
+        _showToast(context, details.globalPosition);
+        await Clipboard.setData(
+          ClipboardData(text: widget.content),
+        );
+      },
+      child: widget.child,
+    );
+  }
 }
