86ee1b4a5d3a9a7b3694157f3c55048366bcfd41
miguelgarciak <miguel@eucalyptuslabs.com>
2023-05-10 05:48:33 -0600

    Fix derived titles

--- lib/pages/home/widgets/derived_accounts.dart
+++ lib/pages/home/widgets/derived_accounts.dart
@@ -17,122 +17,118 @@ class DerivedAccounts extends StatelessWidget {
         builder: (context) => Column(
           children: [
             RoundedContainer(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 15),
-                    child: Wrap(
-                      children: [
-                        const Text(
-                          "${Strings.derived} ",
-                          style: Styles.textStyleHeader4,
-                        ),
-                        Text(
-                          Strings.accounts.toLowerCase(),
-                          style: Styles.textStyleHeader4,
-                        ),
-                      ],
-                    ),
+              child: LayoutBuilder(builder: (ctx, constraints) {
+                final isMobile = constraints.maxWidth < Sizes.small;
+                final accountsText = Text(
+                  Strings.accounts,
+                  style: Styles.textStyleSubheading.copyWith(
+                    color: CustomColors.light100,
                   ),
-                  LayoutBuilder(
-                    builder: (ctx, constraints) {
-                      final isMobile = constraints.maxWidth < Sizes.small;
-                      final accountsText = Text(
-                        Strings.accounts,
-                        style: Styles.textStyleSubheading.copyWith(
-                          color: CustomColors.light100,
-                        ),
-                      );
-                      final addressText = Text(
-                        Strings.address,
-                        style: Styles.textStyleSubheading.copyWith(
-                          color: CustomColors.light100,
-                        ),
-                      );
-                      final privateKeyText = Text(
-                        Strings.privateKeyTitle,
-                        style: Styles.textStyleSubheading.copyWith(
-                          color: CustomColors.light100,
-                        ),
-                      );
-                      return Column(
-                        children: [
-                          if (isMobile)
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 16),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  accountsText,
-                                  addressText,
-                                  privateKeyText,
-                                ],
-                              ),
-                            ),
-                          if (!isMobile)
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 16),
-                              child: Row(
-                                children: [
-                                  Flexible(
-                                    flex: 10,
-                                    fit: FlexFit.tight,
-                                    child: accountsText,
-                                  ),
-                                  Flexible(
-                                    flex: 45,
-                                    fit: FlexFit.tight,
-                                    child: addressText,
-                                  ),
-                                  Flexible(
-                                    flex: 45,
-                                    fit: FlexFit.tight,
-                                    child: privateKeyText,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          Container(
-                            margin: const EdgeInsets.only(
-                              top: 23,
-                              bottom: 18,
-                            ),
-                            child: Divider(
-                              color: CustomColors.light10,
-                            ),
-                          ),
-                          if (homePageStore.keys.isNotEmpty)
-                            SizedBox(
-                              height: 480,
-                              child: ListView.builder(
-                                itemBuilder: (context, index) {
-                                  final item = homePageStore.keys[index];
-                                  return DerivedAccountItem(
-                                    index: index,
-                                    result: item,
-                                  );
-                                },
-                                itemCount: homePageStore.keys.length,
-                              ),
-                            ),
-                        ],
-                      );
-                    },
+                );
+                final addressText = Text(
+                  Strings.address,
+                  style: Styles.textStyleSubheading.copyWith(
+                    color: CustomColors.light100,
                   ),
-                  if (homePageStore.keys.isNotEmpty)
+                );
+                final privateKeyText = Text(
+                  Strings.privateKeyTitle,
+                  style: Styles.textStyleSubheading.copyWith(
+                    color: CustomColors.light100,
+                  ),
+                );
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
                     Container(
-                      margin: const EdgeInsets.only(top: 20),
-                      child: GenerateButton(
-                        title: Strings.generateMore,
-                        onPressCallback: () {},
+                      width: double.infinity,
+                      margin: const EdgeInsets.only(bottom: 15),
+                      child: Wrap(
+                        children: [
+                          const Text(
+                            "${Strings.derived} ",
+                            style: Styles.textStyleHeader4,
+                          ),
+                          Text(
+                            Strings.accounts.toLowerCase(),
+                            style: Styles.textStyleHeader4,
+                          ),
+                        ],
                       ),
                     ),
-                ],
-              ),
+                    Column(
+                      children: [
+                        if (isMobile)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                accountsText,
+                                addressText,
+                                privateKeyText,
+                              ],
+                            ),
+                          ),
+                        if (!isMobile)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  flex: 10,
+                                  fit: FlexFit.tight,
+                                  child: accountsText,
+                                ),
+                                Flexible(
+                                  flex: 45,
+                                  fit: FlexFit.tight,
+                                  child: addressText,
+                                ),
+                                Flexible(
+                                  flex: 45,
+                                  fit: FlexFit.tight,
+                                  child: privateKeyText,
+                                ),
+                              ],
+                            ),
+                          ),
+                        Container(
+                          margin: const EdgeInsets.only(
+                            top: 23,
+                            bottom: 18,
+                          ),
+                          child: Divider(
+                            color: CustomColors.light10,
+                          ),
+                        ),
+                        if (homePageStore.keys.isNotEmpty)
+                          SizedBox(
+                            height: 480,
+                            child: ListView.builder(
+                              itemBuilder: (context, index) {
+                                final item = homePageStore.keys[index];
+                                return DerivedAccountItem(
+                                  index: index,
+                                  result: item,
+                                );
+                              },
+                              itemCount: homePageStore.keys.length,
+                            ),
+                          ),
+                      ],
+                    ),
+                    if (homePageStore.keys.isNotEmpty)
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: GenerateButton(
+                          title: Strings.generateMore,
+                          onPressCallback: () {},
+                        ),
+                      ),
+                  ],
+                );
+              }),
             ),
             const SizedBox(height: 24),
           ],
