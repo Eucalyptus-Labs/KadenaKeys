fafd1c2a42cf7788f53d6b14cda7c61a042f4e23
Varsik Nikoyan <varsik@eucalyptuslabs.com>
2023-06-08 13:30:33 +0400

    deps updated

--- lib/utils/key_derivers/derive_seed/vm/derive_seed_worker.dart
+++ lib/utils/key_derivers/derive_seed/vm/derive_seed_worker.dart
@@ -4,4 +4,4 @@ import 'package:squadron/squadron_service.dart';
 
 import '../derive_seed_service.dart';
 
-void start(Map command) => run((startRequest) => SeedExtractionServiceImpl(), command);
+void start(List command) => run((startRequest) => SeedExtractionServiceImpl(), command);
