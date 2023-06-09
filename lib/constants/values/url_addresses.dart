108a4d6c20e448852321b10e61107cfe5a03641e
miguelgarciak <miguel@eucalyptuslabs.com>
2023-06-09 05:42:43 -0600

    Fix 1890 and 1946
    - Fix key and address truncated
    - Add License to repo and link

--- lib/constants/values/url_addresses.dart
+++ lib/constants/values/url_addresses.dart
@@ -1,8 +1,10 @@
 part of 'values.dart';
 
 class Url {
-  static const String readMe =
-      'https://github.com/Eucalyptus-Labs/KadenaKeys#readme';
+  // static const String readMe =
+  //     'https://github.com/Eucalyptus-Labs/KadenaKeys#readme';
+  static const String license =
+      'https://github.com/Eucalyptus-Labs/KadenaKeys/blob/master/LICENSE';
   static const String koalaWallet = 'https://koalawallet.io';
   static const String kdaExplorer = 'https://kdaexplorer.com';
   static const String kadenaEcosystem = 'https://kadenaecosystem.com';
