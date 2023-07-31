b39ad4064dae10e938cf41ce9422944f9df6a248
Yaroslav <yaroslav@eucalyptuslabs.com>
2023-07-31 14:54:47 +0300

    APP-2193 - Removed unused url, bumped lib versions.

--- lib/constants/values/url_addresses.dart
+++ lib/constants/values/url_addresses.dart
@@ -2,7 +2,6 @@ part of 'values.dart';
 
 class Url {
   static const String youtubeTutorial = 'https://www.youtube.com/watch?v=VOSJ7ylpIb0';
-  static const String readme = 'https://github.com/Eucalyptus-Labs/KadenaKeys/blob/master/README.md';
   static const String license = 'https://github.com/Eucalyptus-Labs/KadenaKeys/blob/master/LICENSE';
   static const String koalaWallet = 'https://koalawallet.io';
   static const String kdaExplorer = 'https://kdaexplorer.com';
